568d7b9b071eb47d3fb71edde875925bd6c9cf77
ArtemiiFrolov <arfr@live.ru>
2017-04-24 14:40:39 +0300

    WorkingLive. Last line fixed. Added maximums and count of maximums.

--- OpenCVTest2/OpenCVTest2.cpp
+++ OpenCVTest2/OpenCVTest2.cpp
@@ -17,6 +17,7 @@ double minVal = 0;
 int e_slider, e_slidermax, x_slider, x_slidermax, y_slider, y_slidermax, a_slider, a_slidermax;
 int savedX = 0;
 int savedY = 0;
+int countMax = 0;
 
 Mat frame;
 Mat dst;
@@ -35,8 +36,8 @@ int main(int argc, const char** argv)
 	//namedWindow("original", 1);
 	//namedWindow("threshold", 1);
 	namedWindow("output",1);
-	VideoCapture cap("Test.mp4");
-	
+	//VideoCapture cap("Test2.mp4");
+	VideoCapture cap("rtsp://192.168.5.4/live.sdp");
 	cap >> frame;
 	//trackbar
 	e_slider = 100;
@@ -53,7 +54,7 @@ int main(int argc, const char** argv)
 	createTrackbar("Y", "output", &y_slider, y_slidermax, on_trackbar);
 	createTrackbar("a", "output", &a_slider, a_slidermax, on_trackbar);
 	createTrackbar("Epsilon", "output", &e_slider, e_slidermax, on_trackbar);
-	for (;;)
+	while (cap.isOpened())
 	{
 		cap >> frame;
 		if (frame.empty()) 
@@ -86,7 +87,10 @@ int main(int argc, const char** argv)
 			int firstRow = 0;
 			int lastRow = 0;
 			int maxRow = 0;
+			int maxRow2 = 0;
 			int maxRowI = 0;
+			int maxRowI2 = 0;
+			countMax = 0;
 			bool flagFirst = true;
 			bool flagLast = false;
 			cvtColor(dst, dst2, COLOR_RGB2GRAY);
@@ -99,6 +103,13 @@ int main(int argc, const char** argv)
 					Scalar intensity = dst2.at<uchar>(i, j);
 					if (intensity.val[0] > 0) {
 						avgX += intensity.val[0];
+						if (intensity.val[0] == maxVal){
+							Vec3b & color = dst.at<Vec3b>(i, j);
+							color[1] = 255;
+							color[2] = 255;
+							color[0] = 0;
+							countMax++;
+						}
 						avgN++;
 					}
 				}
@@ -115,22 +126,34 @@ int main(int argc, const char** argv)
 						maxRow = avgRow[i];
 						maxRowI = i;
 					}
+					if (avgRow[i] == maxRow)
+					{	
+						maxRow2 = maxRow;
+						maxRowI2 = i;
+					}
 					flagLast = true;
 					
 				}
 				else {
 					avgRow[i] = 0;
-					if (flagLast)
+					if (flagLast) 
 					{
 						lastRow = i - 1;
 						flagLast = false;
 					}
 				}
 			}
+			if (flagLast)
+			{
+				lastRow = a_slider;
+			}
 			graphic = Mat::zeros(dst.rows, 255, CV_8UC3);
 			for (int i = 0; i < dst.rows; i++) {
-				line(graphic, Point(0, i), Point(avgRow[i] * 2, i), Scalar(0, 55, 255), 1, 8, 0);
+				line(graphic, Point(0, i), Point(avgRow[i], i), Scalar(0, 55, 255), 1, 8, 0);
 			}
+			if (maxRow == maxRow2){
+				maxRowI = (maxRowI + maxRowI2) / 2;
+			} 
 			line(graphic, Point(0, (lastRow + firstRow) / 2), Point(255, (lastRow + firstRow) / 2), Scalar(55, 255, 0), 1, 8, 0);
 			line(graphic, Point(0, maxRowI), Point(255, maxRowI), Scalar(255, 55, 0), 1, 8, 0);
 			
@@ -144,101 +167,22 @@ int main(int argc, const char** argv)
 		Size s = frame.size();
 		Size s1 = dst.size();
 		Size s2 = graphic.size();
-		Mat output(s.height+s1.height, max(s.width,s1.width+s2.width), frame.type());
-		Mat help1(output, Rect(0, 0, s.width, s.height));
+	//	Mat output(s.height+s1.height, max(s.width,s1.width+s2.width), frame.type());
+		/*Mat help1(output, Rect(0, 0, s.width, s.height));
 		Mat help2(output, Rect(0, s.height, s1.width, s1.height));
-		Mat help3(output, Rect(a_slider, s.height, s2.width, s1.height));
+		Mat help3(output, Rect(a_slider, s.height, s2.width, s1.height));*/
+		Mat output(s.height, s.width+s1.width + s2.width, frame.type());
+		Mat help1(output, Rect(0, 0, s.width, s.height));
+		Mat help2(output, Rect(s.width, 0, s1.width, s1.height));
+		Mat help3(output, Rect(s.width+a_slider, 0, s2.width, s1.height));
+
 		frame.copyTo(help1);
 		dst.copyTo(help2);
 		graphic.copyTo(help3);
 
-
-
-		/*
-
-		//размываем изображение (тут чтобы избавиться от шума нужна большая фильтрация)
-		GaussianBlur(dst, dst, Size(5, 5),0,0);
-		//ищем яркие пиксели
-		minMaxLoc(dst, &minVal, &maxVal);
-		threshold(dst, dst, maxVal - e_slider, maxVal, 3);
-		//Canny
-	
-		int avgX = 0;
-		int avgY = 0;
-		int avgN = 0;
-		//проверяем выход за рамки
 		
-		//начальное условие
-		if (savedX == 0) {
-			for (int i = 0; i < dst.cols; i++)
-				for (int j = 0; j < dst.rows; j++)
-				{
-					Vec3b intensity = dst.at<Vec3b>(j, i);
-					if ((intensity.val[0] > 0) || (intensity.val[1] > 0) || (intensity.val[2] > 0)) {
-						avgX += j;
-						avgY += i;
-						avgN++;
-					}
-				}
-		}//поиск внутри прямоугольника
-		else {
-			if (savedX + a_slider > dst.rows) {
-				a_slider = dst.rows - savedX;
-			}
-			if (savedY + b_slider > dst.cols) {
-				b_slider = dst.cols - savedY;
-			}
-			if (savedX - a_slider <0) {
-				a_slider = savedX;
-			}
-			if (savedY - b_slider <0) {
-				b_slider = savedX;
-			}
-			for (int i = savedY - b_slider; i < savedY + b_slider + 1; i++)
-				for (int j = savedX - a_slider; j < savedX + a_slider; j++)
-				{
-					Vec3b intensity = dst.at<Vec3b>(j, i);
-					if ((intensity.val[0] > 0) || (intensity.val[1] > 0) || (intensity.val[2] > 0)) {
-						avgX += j;
-						avgY += i;
-						avgN++;
-					}
-				}
-		}
-		//поиск среднего
-		if (avgN != 0) 
-		{
-			avgX = avgX / avgN;
-			avgY = avgY / avgN;			
-		}
-		savedX = avgX;
-		savedY = avgY;
-		//рисуем прямоугольник вокруг точки
-		int a0 = savedX - a_slider;
-		int b0 = savedY - b_slider;
-		int a1 = savedX + a_slider;
-		int b1 = savedY + b_slider;
-		rectangle(dst, Point(b0,a0), Point(b1, a1), Scalar(0, 55, 255), +1, 8);
+		putText(output, to_string(countMax), cvPoint(s.width+10, a_slider+10),	FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200, 200, 250), 1, CV_AA);
 		
-		if (savedX>0)
-		for (int i = 0; i < 9; i++) {
-			Vec3b &intensity = dst.at<Vec3b>(avgX-1+i%3, avgY-1+i/3);
-			intensity.val[1] = 55;
-			intensity.val[2] = 255;
-		}
-		char result[100];
-		sprintf_s(result,  "X is %d, Y is %d", avgY, avgX);
-		putText(dst, result, cvPoint(30, 30),	FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200, 200, 250), 1, CV_AA);
-		*/
-
-
-
-
-		
-
-
-
-
 
 		//показываем кадр
 		//imshow("original", frame);
